8e42fa9f009e9ea4e47e410d043fa50cff4b1e2f
Y-Shivansh <sharma.shivansh1305@gmail.com>
2024-12-08 11:04:14 +0530

    backend edit

--- server/middlewares/admin.middleware.js
+++ server/middlewares/admin.middleware.js
@@ -4,8 +4,8 @@ import { SECRET_KEY } from '../config/jwt.js'
 export const verifyAdminToken = async(req,res,next) => {
     try{
         const {token} = req.cookies;
-        if(!token) return res.status(403).json({message: "Access denied. No token provided."});
         console.log(token);
+        if(!token) return res.status(403).json({message: "Access denied. No token provided."});
         jwt.verify(token, SECRET_KEY, (err,decoded) => {
             if(err){
                 res.status(403).json({message: "Invalid or Forbidden Token", err})

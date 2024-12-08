f94cd53c2a37a584dbd6f4128d179f4f8201c81c
Y-Shivansh <sharma.shivansh1305@gmail.com>
2024-12-08 15:30:31 +0530

    backend edit

--- server/app.js
+++ server/app.js
@@ -9,7 +9,7 @@ import cookieParser  from 'cookie-parser';
 
 const app = express();
 const corsOption = {
-    origin: "https://review-system24x7.vercel.app/api",
+    origin: "https://review-system24x7.vercel.app",
     credentials: true
 }
 app.use(express.json());
@@ -22,7 +22,7 @@ app.use('/api/v1', userRoutes);
 
 // Admin Routes
 app.post('/api/admin/login', adminLoginController)
-// app.use(verifyAdminToken);
+app.use(verifyAdminToken);
 app.use('/api/admin', adminRoutes)
 
 export default app
